3ddd2278ca80618654261748eb9cb0501fd5754b
yao <ywang305@syr.edu>
2021-01-30 00:58:03 -0500

    Initial Commit

---
+++ YuooCoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YuooCoin
+//
+//  Created by Yao Wang on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YuooCoin/YuooCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuooCoinApp.swift
+//  YuooCoin
+//
+//  Created by Yao Wang on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct YuooCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
