38e62a9014be0f142d15167e6ab8de825b1e3477
yao <ywang305@syr.edu>
2021-04-20 20:19:27 -0400

    Tabs

---
+++ YuooCoin/views/tabs/Tabs.swift
@@ -0,0 +1,31 @@
+//
+//  SwiftUIView.swift
+//  YuooCoin
+//
+//  Created by Yao Wang on 4/20/21.
+//
+
+import SwiftUI
+
+struct Tabs: View {
+    @State private var selectedTab = 1
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            TickerBoard()
+                .tabItem { Image(systemName: "chart.bar").tag(1) }
+            Text("2nd Tab Content")
+                .tabItem { Image(systemName: "link") }
+                .tag(2)
+            Text("user")
+                .tabItem { Image(systemName: "person")
+                    .tag(3)
+                }
+        }
+    }
+}
+
+struct Tabs_Previews: PreviewProvider {
+    static var previews: some View {
+        Tabs()
+    }
+}
